26947442a3c0b55c1c2aac47b8096093c01f47b7
Alikber <alikber96@mail.ru>
2022-07-14 20:00:23 +0300

    Initial Commit

---
+++ The Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light
+//
+//  Created by Аликбер Гюлмагомедов on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
